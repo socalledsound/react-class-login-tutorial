c566a1c4cc3ade32a602bf5c757a15801ad6e8e9
Chris Kubick <socalledsound@gmail.com>
2021-06-28 09:41:21 -0700

    routing

---
+++ src/pages/SignInPage/SignInPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignInPage = () => {
+    return ( 
+        <div>
+            sign in page will go here
+        </div>
+     );
+}
+ 
+export default SignInPage;
